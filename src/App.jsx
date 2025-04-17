9b489ff8422b793ef4564afe3340f0bdeaf35381
Rohit Patil <rohitrajupatil12@gmail.com>
2025-04-17 16:55:57 +0530

    make the router of the projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes ,Route } from "react-router-dom";
+import Layout from "./assets/layouts/Layout";
+import About from "./assets/pages/About";
 
 function App() {
   return (
-   <>
-   <h1>rohit patil is king</h1>
-   </>
-  )
+    // <Routes>
+    //   <Route path="/" element={<Layout/>} />
+      
+
+      
+      
+    // </Routes>
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      {/* All pages that use the same Navbar go here */}
+      {/* <Route index element={<Home />} /> */}
+      <Route path="about" element={<About />} />
+    </Route>
+  </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
+import "../components/navbar.css";
 
-// import the image 
-import myImage from '../assets/navbar.png';
+import Contact from "../assets/pages/Contact";
+import About from "../assets/pages/About";
+
+// import form the react router dom
+import { Link } from "react-router-dom";
+
+// import the image
+import myImage from "../assets/navbar.png";
 
 function Navbar() {
   return (
     <>
+      <div className="contianer">
+        <img src={myImage} alt="" />
+        <div className="menu-contianer">
+          <Link to="/about" className="navlink"> About</Link>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
