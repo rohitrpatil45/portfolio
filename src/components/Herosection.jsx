3af1d75b9f4a0545037fbc692d109b0ef7d5d367
Rohit Patil <rohitrajupatil12@gmail.com>
2025-04-22 14:00:42 +0530

    add the working socail media icon

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "../components/herosection.css";
 
-function Herosection() {
+// import form the react router dom
+import { Link } from "react-router-dom";
+// import icon form the react icon
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 
-  const words = ['Javascript','Nodejs','React' ,"Frontend" ,"backend",]
-  const [correntWord , setCurrentWord] = useState(words[0])
-  const [index , setindex] = useState(0)
+function Herosection() {
+  const words = ["Javascript", "Nodejs", "React", "Frontend", "backend"];
+  const [correntWord, setCurrentWord] = useState(words[0]);
+  const [index, setindex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setindex((prevalue) => (prevalue + 1) % words.length)
-      setCurrentWord(words[index])
+      setindex((prevalue) => (prevalue + 1) % words.length);
+      setCurrentWord(words[index]);
       // console.log(correntWord)
-      
     }, 2000);
     return () => {
-      clearInterval(interval)
-      
+      clearInterval(interval);
     };
   }, [words]);
   return (
@@ -35,9 +38,33 @@ function Herosection() {
               <span className="changing-text-container"> {correntWord}</span>
             </span>
           </div>
+          <div className="paragraph-container">
+            <span className="details-span">
+              Welcome to my portfolio! I hope you enjoy your visit. I specialize
+              in Back-End development with Node.js and Front-End development
+              with React.js. I have worked on various projects, including a
+              Real Link-Shortener, E-Commerce APIs, React-based Website Design
+              and Development, and more. I am passionate about learning new
+              technologies and building solutions that make a difference. Thank
+              you for visiting my portfolio! I hope you like my work. Happy
+              coding!
+            </span>
+          </div>
+          <div className="social-median-contianer">
+            <Link to="https://github.com/rohitrpatil45">
+              <FaGithub className="icon" />
+            </Link>
+            <Link to="https://www.linkedin.com/in/rohit-patil-56a634247?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+              <FaLinkedin className="icon" />
+            </Link>
+            <Link to="https://x.com/RohitPatil82428?s=09">
+              <BsTwitterX className="icon" />
+            </Link>
+          </div>
         </div>
+
         <div className="mainright">
-          f
+          <h2>roih</h2>
         </div>
       </div>
     </>
