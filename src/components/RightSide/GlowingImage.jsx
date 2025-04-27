2ac9dbe35c187145851853c2247e0232c10ffc68
Rohit Patil <rohitrajupatil12@gmail.com>
2025-04-27 21:41:37 +0530

    this new life after the error

---
+++ src/components/RightSide/GlowingImage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../RightSide/GlowingImage.css'
+
+function GlowingImage() {
+  return (
+   <>
+     <div className="box">
+        <div className="inner-box">
+
+        </div>
+        
+    </div>
+
+   </>
+  )
+}
+
+export default GlowingImage
