d50fd65293da89b2192385d2123350eb5cc4db60
Rohit Patil <rohitrajupatil12@gmail.com>
2025-08-25 14:08:49 +0530

    working on the project section

--- src/components/project_section/PorjectGrid.jsx
+++ src/components/project_section/PorjectGrid.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import { motion } from "framer-motion";
-import '../project_section/projectgrid.css'
+import React, { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+import "../project_section/projectgrid.css";
+import ProjectGirdColumns from "./ProjectGirdColumns";
 
 function PorjectGrid() {
-  return (
-<>
-<div className='motion-animation'>
-  <motion.h1
-      initial={{ opacity: 0, y: -50 }}   // starting state
-      animate={{ opacity: 1, y: 0 , rotate : -2}}     // final state
-      transition={{ duration: 1 }}       // animation timing
-    className='motion-h1'
-    >
-      Projects 🚀
-    </motion.h1>
-</div>
+ const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.3 });
 
-</>
-)
+  return (
+    <>
+      <div className="motion-animation">
+        <motion.h1
+          ref={ref}
+          initial={{ opacity: 0, y: -50 }}   /* starting state */
+          animate={isInView ? { opacity: 1, y: 0, rotate: -2 } : {}} /* final state */
+          transition={{ duration: 1 }}       /* animation timing */
+          className="motion-h1"
+        >
+          Projects 🚀
+        </motion.h1>
+      </div>
+
+      <ProjectGirdColumns />
+    </>
+  );
 }
 
 export default PorjectGrid
---
+++ src/components/project_section/ProjectGirdColumns.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import "../project_section/projectgrid.css";
+
+function ProjectGirdColumns() {
+  return( 
+  <div className="main-boxgrid">
+   
+   
+  <h2>rohit</h2>
+  
+  </div>
+  );
+}
+
+export default ProjectGirdColumns;
