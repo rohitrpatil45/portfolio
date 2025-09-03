a1afdc06c6ac9ccff6e0171df517f62c99e3b000
Rohit Patil <rohitrajupatil12@gmail.com>
2025-09-03 19:52:06 +0530

    contact page edit

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../pages/contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <h2>This Cantact page is under construction</h2>
+      <p>For The Contact Details Go to Footer Sectio</p>
+    </div>
   )
 }
 
