6a1a3869baf1e69896b819d76e8175e70721406b
Rohit Patil <rohitrajupatil12@gmail.com>
2025-09-10 15:29:04 +0530

    add the new project in file

--- src/components/project_section/GridContainer.jsx
+++ src/components/project_section/GridContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import projectdp from '../../assets/homepage.png';
+import fancynamedp from '../../assets/fancyname.png';
 import "../project_section/GridContainer.css";
 
 
@@ -13,6 +14,14 @@ function GridContainer() {
         description="A free AI-powered image editor with tools for background removal, recoloring, aspect ratio resizing, and image generation."
         demoLink="https://kokan-ratnadevelopers.vercel.app/"
         githubLink="https://github.com/rohitrpatil45/kokanRatnadevelopers"
+      />
+
+         <ProjectCard
+        image={fancynamedp}
+        title="FF FancyName Generator"
+        description="A lightweight name generator that produces elegant, fancy, and creative names for branding, apps, and personal use."
+        demoLink="https://ffstylishname.vercel.app/"
+        githubLink="https://github.com/rohitrpatil45/ffstylishname"
       />
      
       
